a6877a1b7f6d8fec71cdbef5074e885455002146
Denys <chornijj14@gmail.com>
2020-05-25 18:21:19 +0200

    Set setinterval to update data each 3s

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ let cryptoTableBody = document.querySelector('.crypto-tbody');
 const cryptoLink = 'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD';
 const imgUrl = 'https://www.cryptocompare.com/';
 
-function getData(link, callback){
+let loading = `<div class="spinner-grow " role="status">
+<span class="sr-only justify-content-center text-center align-middle" >Loading...</span>
+</div>`;
+
+function getData(link , callback){
     let xhr = new XMLHttpRequest();
     xhr.open('GET', link, true);
     xhr.onreadystatechange = () =>{
@@ -11,25 +15,32 @@ function getData(link, callback){
         }
     }
     xhr.send();
+    console.log('hey');
 }
-getData(cryptoLink, renderLine);
+// getData(cryptoLink, renderLine);
+setInterval(()=>{
+    getData(cryptoLink, renderLine);
+}, 3000);
 
 function renderLine(data){
     let line = ``;
-    for(let i = 0; i<data.length; i++){
-        line += `
-            <tr>
-                <th scope="row">${i+1}</th>
-                <td><img src="${imgUrl + data[i].CoinInfo.ImageUrl}" class="text-left crypto-img"/>${data[i].CoinInfo.Name}</td>
-                <td>${data[i].CoinInfo.FullName}</td>
-                <td>${data[i].DISPLAY.USD.PRICE}</td>
-                <td>${(data[i].DISPLAY.USD.MKTCAP)}</td>
-            </tr>
-        `;
-    }
-    cryptoTableBody.innerHTML = line;
-    console.log(data[0]);
-}
+    if(data.length === undefined){
+        cryptoTableBody.innerHTML = loading;
 
+    }else{
+        for(let i = 0; i<data.length; i++){
+            line += `
+                <tr>
+                    <th scope="row">${i+1}</th>
+                    <td><img src="${imgUrl + data[i].CoinInfo.ImageUrl}" class="text-left crypto-img"/>${data[i].CoinInfo.Name}</td>
+                    <td>${data[i].CoinInfo.FullName}</td>
+                    <td>${data[i].DISPLAY.USD.PRICE}</td>
+                    <td>${(data[i].DISPLAY.USD.MKTCAP)}</td>
+                </tr>
+            `;
+        }
+        cryptoTableBody.innerHTML = line;
+    }
+}
 
 
